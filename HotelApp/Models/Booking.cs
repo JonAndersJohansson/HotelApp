ba6d04959ea99554216b14b22673fbae8bd3d7db
Jon Johansson <jonjohansson88@gmail.com>
2024-12-08 18:11:02 +0100

    Booking klar

--- HotelApp/Models/Booking.cs
+++ HotelApp/Models/Booking.cs
@@ -2,6 +2,21 @@
 {
     public class Booking
     {
+        public int BookingId { get; set; }
+        public Customer CustomerInBooking { get; set; }
+        public short RoomNumber { get; set; }
+        public List<Room> ListOfRoomsInBooking { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte NumberOfGuests { get; set; }
+        public bool IsCurrentlyStaying { get; set; } = false;
+        public bool IsCancelled { get; set; } = false;
+        public Invoice InvoiceInBooking { get; set; }
+
+        public override string ToString()
+        {
+            return $"BookingId: {BookingId}, Customer: {CustomerInBooking.LastName}, RoomNr: {RoomNumber}, StartDate: {StartDate:yyyy-MM-dd}, EndDate: {EndDate:yyyy-MM-dd}, IsCancelled: {IsCancelled}";
+        }
     }
 
 }
